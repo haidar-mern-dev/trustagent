8cafccdee2c17ee5430a189acb5e781c89e8e349
Haidar ali <haidar.ali@txlabz.com>
2024-08-02 11:43:49 +0500

    rentForm design and state implementaion

--- src/components/onboard/index.js
+++ src/components/onboard/index.js
@@ -1,46 +1,76 @@
-import React, { useState } from "react";
-import OtpInput from "react-otp-input";
-import { Link, useLocation } from "react-router-dom";
+// src/OnBoardPage.js
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setPage } from "../../redux/formSlice";
 import Tutorial from "./pages/Tutorial";
 import PropertyForm from "./pages/RentForm";
 
 const OnBoardPage = () => {
-  const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
+  const page = useSelector((state) => state.form.page);
 
   const handleNext = () => {
-    setPage((prev) => prev + 1);
+    dispatch(setPage(page + 1));
   };
 
   const handleBack = () => {
-    setPage((prev) => prev - 1);
+    dispatch(setPage(page - 1));
+  };
+
+  const renderPage = () => {
+    switch (page) {
+      case 1:
+        return <Tutorial />;
+      case 2:
+        return <PropertyForm />;
+      case 3:
+        return <></>;
+      case 4:
+        return <></>;
+      default:
+        return <> </>;
+    }
+  };
+
+  const getProgressWidth = () => {
+    switch (page) {
+      case 1:
+        return "20%";
+      case 2:
+        return "40%";
+      case 3:
+        return "60%";
+      case 4:
+        return "80%";
+      default:
+        return "20%";
+    }
   };
 
   return (
-    <div className="min-h-screen  bg-white md:p-20 p-4 main_container bg-cover bg-center">
+    <div className="min-h-screen bg-white md:p-20 p-4 main_container bg-cover bg-center">
       <div className="w-full bg-gray-300 rounded-full h-2.5 mb-10">
         <div
           className="bg-theme_color h-2.5 rounded-full"
-          style={{ width: "20%" }}
+          style={{ width: getProgressWidth() }}
         />
-
-        <div className="text-right text-sm font-medium mt-4">20% Completed</div>
+        <div className="text-right text-sm font-medium mt-4">
+          {getProgressWidth()} Completed
+        </div>
       </div>
-      {/* <Tutorial /> */}
-      <PropertyForm />
-      <div className=" py-4 px-8 flex justify-end gap-10 items-center">
-        {page > 1 ? (
+      {renderPage()}
+      <div className="py-4 px-8 flex justify-end gap-10 items-center">
+        {page > 1 && (
           <button
             onClick={handleBack}
             className="bg-theme_color text-white px-10 py-4 rounded"
           >
             Back
           </button>
-        ) : (
-          <div></div>
         )}
         <button
           onClick={handleNext}
-          className="bg-theme_color text-white px-10 py-4  rounded"
+          className="bg-theme_color text-white px-10 py-4 rounded"
         >
           Next
         </button>

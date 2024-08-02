f34e09a37e7154eedc8424182b687a55c433a899
Rana Adnan <rana.gul755@gmail.com>
2024-08-02 23:23:06 +0500

    designing new pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Signup from "./pages/signup";
 import Otp from "./pages/otp";
 import OnBoard from "./pages/onboard";
 import DashboardPage from "./pages/dashboard";
+import ListingDetails from "./components/AgentFlow/ListingDetails";
+import Bidding from "./components/AgentFlow/Bidding";
+import ChatSupport from "./components/AgentFlow/ChatSupport";
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Route path="/otp" element={<Otp />} />
         <Route path="/onboard" element={<OnBoard />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/listing-details" element={<ListingDetails />} />
+        <Route path="/bidding-submission" element={<Bidding />} />
+        <Route path="/help-chat" element={<ChatSupport />} />
       </Routes>
     </Router>
   );
